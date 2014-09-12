53c2c9c4a4e07325241d776bbd61851976494dd8
Anthony Cameron <anthony.cameron@jadedpixel.com>
2014-09-12 16:40:48 -0400

    use a setup do

--- test/browser/full_page_refresh_test.rb
+++ test/browser/full_page_refresh_test.rb
@@ -4,22 +4,22 @@ require 'benchmark'
 class FullPageRefreshTest < ActionDispatch::IntegrationTest
   include Capybara::DSL
 
-  test "will strip noscript tags" do
+  setup do
     visit "/pages/1"
     click_link "Perform a full refresh"
+  end
+
+  test "will strip noscript tags" do
     refute page.has_content?("Please enable JavaScript")
   end
 
   test "will replace the title and body" do
-    visit "/pages/1"
-    click_link "Perform a full refresh"
     assert_equal "Sample static HTML", page.title
     assert page.has_selector?("body.hot-new-bod")
   end
 
   test "will execute scripts that do not have data-turbolinks-eval='false'" do
-    visit "/pages/1"
-    click_link "Perform a full refresh"
     assert page.has_content?("Hi there, from a script!")
+    refute page.has_content?("Not going to see me, turbolinks will ignore")
   end
 end
