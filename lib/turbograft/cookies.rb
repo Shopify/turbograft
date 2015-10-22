8f21748123fb442cfd9e0cc101f4b70a0ba236ac
Willem van Bergen <willem@vanbergen.org>
2015-10-22 08:34:33 -0400

    Don’t set request_method cookie for GET requests.
    The presence of a Set-Cookie header prevents CDNs from caching a page, so we want to prevent cookies from being set unnecessarily. Turbolinks/Turbograft will assume a request is a GET request if the cookie is not set, so doesn’t doesn’t change Turbograft’s behaviour. 
    
    This mirrors https://github.com/rails/turbolinks/commit/62cc3db457ad9745ccda047dd43ab84fb3d89707

--- lib/turbograft/cookies.rb
+++ lib/turbograft/cookies.rb
@@ -1,10 +1,14 @@
 module TurboGraft
   # Sets a request_method cookie containing the request method of the current request.
-  # The Turbolinks script will not initialize if this cookie is set to anything other than GET.
+  # The Turbolinks script will not initialize if this cookie is not set, or set to anything other than GET.
   module Cookies
     private
       def set_request_method_cookie
-        cookies[:request_method] = request.request_method
+        if request.get?
+          cookies.delete(:request_method)
+        else
+          cookies[:request_method] = request.request_method
+        end
       end
   end
 end
