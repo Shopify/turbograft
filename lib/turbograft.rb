9ca615f0a7478025aced824beed1cad973433a64
Peter Schröder <phoetmail@googlemail.com>
2016-08-11 10:04:21 +0200

    prepend is public since 2.1
    since the method has been made public, we can invoke it without any `moduel_eval`ing

--- lib/turbograft.rb
+++ lib/turbograft.rb
@@ -35,8 +35,7 @@ module TurboGraft
       end
 
       ActiveSupport.on_load(:action_view) do
-        (ActionView::RoutingUrlFor rescue ActionView::Helpers::UrlHelper).module_eval do
-          prepend XHRUrlFor
+        ActionView::RoutingUrlFor.prepend XHRUrlFor
         end
       end unless RUBY_VERSION =~ /^1\.8/
     end
